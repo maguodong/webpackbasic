5fcd04c1edc0ea638ef79bf1d1acdc11da830900
gary <gary@192.168.0.105>
2018-01-13 16:09:29 +0800

    初始化项目

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './images/webpack.png';
+
+function component() {
+    var element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', ' webpack'],'');
+    element.classList.add('hello');
+   
+    var icon = new Image();
+    icon.src = Icon
+
+    element.appendChild(icon);
+    
+    return element;
+}
+
+document.body.appendChild(component());
